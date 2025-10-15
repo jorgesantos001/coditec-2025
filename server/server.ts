56e3c2cb4dc8fdaae1baed38382eb8fd018b313c
Guilherme Souza <thekingguiiz2003@gmail.com>
2025-10-15 19:57:16 -0300

    feat (backend): Configura o CORS

--- server/server.ts
+++ server/server.ts
@@ -5,10 +5,16 @@ import alimentoRouter from "./src/routes/alimento.routes";
 import campanhaRouter from "./src/routes/campanhas.routes";
 import doacaoRouter from "./src/routes/doacao.routes";
 import localidadeRouter from "./config/routes/localidade.router";
+import cors from "cors";
 
 dotenv.config({ path: "../.env" });
 
 const app = express();
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
 
 // Express routes
 app.use(express.json());
